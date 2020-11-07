8a33c5133412dcaef20818963330adb7cbd85084
BaliOng <bemlo2000@gmail.com>
2020-11-06 21:06:31 -0600

    Added different easy and hard levels, tried to fix a glitch with the pumpkin

---
+++ Spooky Shooter/Assets/Scripts/Pumpkin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour
+{
+    public Transform[] TurretTransforms;
+    public float ReloadDelay = 2f;
+	public bool CanFire = true;
+
+    void Update()
+    {
+        if(CanFire && TurretTransforms != null){
+        foreach(Transform T in TurretTransforms)
+				{
+					PumpkinManager.SpawnAmmo(T.position, T.rotation);
+					Debug.Log("Pumpkin ammo fired");
+				}
+        CanFire = false;
+	    Invoke("EnableFire", ReloadDelay);
+        }
+    }
+    void EnableFire()
+	{
+		CanFire = true;
+	}
+}
